5092bf0fde8fc2ac3385f59eb67a0d77e5eb6236
realshovanshah <realshovanshah@gmail.com>
2020-01-11 22:06:44 +0545

    main commit

--- account/urls.py
+++ account/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 app_name = "main"
 urlpatterns = [
-    # path('', views.homepage, name = "homepage"),
+    path('', views.register, name = "register"),
     #path('test/', views.index, name = 'test'),
     path('register/', views.register, name = "register"),
-    path('', views.home, name = "home"),
     path('home/', views.home, name = "home"),
     path('logout/', views.user_logout, name = "logout"),
     path('login/', views.user_login, name = "login")
