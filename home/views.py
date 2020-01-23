2ea55f3aca868fde2156443a96703534362bd0a2
tygbspartan <58836602+tygbspartan@users.noreply.github.com>
2020-01-23 15:59:40 +0545

    search

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Show
+from django.db.models import Q
 # Create your views here.
 
 
 def home(request):
-	return render(request=request, template_name="pages/home.html", context={"shows": Show.objects.all})
+	# return render(request=request, template_name="pages/home.html", context={"shows": Show.objects.all})
+    show = Show.objects.all()
+    if request.GET:
+        query = request.GET['q']
+        show = get_data_queryset(str(query))
+    return render(request, "pages/home.html", {"shows": show})
 
 def upload(request):
     if request.method == 'POST':
@@ -17,3 +23,24 @@ def upload(request):
         return HttpResponse('Thank You For Uploading')
     else:   
         return render(request,'pages/upload.html')
+
+def show_list(request):
+    show = Show.objects.all()
+    if request.GET:
+        query = request.GET['q']
+        show = get_data_queryset(str(query))
+    return render(request, "pages/home.html", {"shows": show})
+
+
+def get_data_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        shows= Show.objects.filter(
+            Q(shows_name__icontains=q) |
+            Q(shows_type__icontains=q) |
+            Q(shows_description__icontains=q)
+        )
+    for show in shows:
+        queryset.append(show)
+    return list(set(queryset))
