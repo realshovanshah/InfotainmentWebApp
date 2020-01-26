128243032f4d5723be172ff76947670a20d8afa3
rujanshakya <58836569+rujanshakya@users.noreply.github.com>
2020-01-26 11:01:04 +0545

    Tested Apps

--- account/tests.py
+++ account/tests.py
@@ -1,2 +1,15 @@
 from django.test import TestCase
 
+# Create your tests here.
+
+class AccountTest(TestCase):
+def setUp(self):
+Account.objects.create(account_name="account", account_body="user", )
+Account.objects.create(account_name="login", account_body="can't login", )
+
+
+def test_ORM(self):
+a1 = Account.objects.get(account_name="account")
+a2 = Account.objects.get(account_name="login")
+self.assertEqual(a1.account_name, "account")
+self.assertEqual(a2.account_name, "can't login")
--- home/tests.py
+++ home/tests.py
@@ -5,10 +5,13 @@ from home.models import Show
 # Create your tests here.
 
 class HeraldTest(TestCase):
-	def setUp(self):
-		Show.objects.create(shows_name="hey", shows_description="hahaha", )
-		# date=datetime.now()
+def setUp(self):
+Show.objects.create(shows_name="hey", shows_description="hahaha", )
+Show.objects.create(shows_name="watch", shows_description="Pleasw wait to watch", )
+# date=datetime.now()
 
-	def test_ORM(self):
-		t = Show.objects.get(shows_name="hey")
-		self.assertEqual(t.shows_name, "hey")
+def test_ORM(self):
+e1 = Show.objects.get(shows_name="hey")
+e2 = Show.objects.get(shows_name="watch")
+self.assertEqual(e1.shows_name, "hey")
+self.assertEqual(e2.shows_name, "Please wait to watch")
